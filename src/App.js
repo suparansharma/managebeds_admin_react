5eac708e7144c2e095bef691ada8a2498b0d3bf8
suparansharma <suparansharmap@gmail.com>
2023-03-20 17:02:08 +0600

    routing set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Layout from './Components/Layout';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Error from './Pages/Error/Error';
 
 function App() {
   return (
     <>
-  <Layout/>
+    <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Layout>
+      </Router>
     </>
   );
 }
